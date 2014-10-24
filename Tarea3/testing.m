c1860cba6f4f014aa8d1cb2b9d7bb551340bcecd
luism <luis.martinezqug.uchile.cl>
2014-10-23 23:35:24 -0300

    Agregado parte para test y filtrate

--- Tarea3/testing.m
+++ Tarea3/testing.m
@@ -2,7 +2,7 @@
 %test path es la ruta de los archivos de entrenamiento
 %centers son los centros de los clusters
 %ej de test_path: C:\Users\Vincent\git\Tarea3_patrones\Tarea3\Dataset\Test
-function [ obtained, expected ] = testing(test_path, svm, centers)
+function [ obtained, expected, scores ] = testing(test_path, svm, centers)
     [perros, otros, expected] = process_img( test_path );
     hist = [histogramas(centers, perros); histogramas(centers, otros)];
     [obtained, scores] = predict(svm,hist);
